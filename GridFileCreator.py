a15eae327137b6897689ddf31fabeef5764b4d5f
kutsukos <kutsukos@csd.uoc.gr>
2024-08-29 15:01:17 +0300

    python3 update

--- GridFileCreator.py
+++ GridFileCreator.py
@@ -1,66 +1,53 @@
-from optparse import OptionParser
+INFILE = 'data.dat'
 
-parser = OptionParser( description="GridFile Creator v0.2.kutsukos This tool is creating a tab-delimited file, that contains positions inside a specified range."
-                                   "It can be used as input in some programs like SweeD or OmegaPlus."
-                                   "\nThe format of this file is \"chr <tab> pos\". It is like a bed file, but it is not defining areas of interest, but points, inside an area." )
-parser.add_option("-c", "--chr", dest="chr",
-                  help="the number of the chr that will be written in output")
-parser.add_option("-s", "--start", dest="start",
-                  help="the starting position of this range. It is included in the file")
-parser.add_option("-e", "--end", dest="end",
-                  help="the ending position of this range. It is included in the file")
-parser.add_option("-n", "--num", dest="num",default=0,
-                  help="the number of positions you want in this range")
-parser.add_option("-d", "--dist", dest="dist",
-                  help="alternative to num, dist is the distance between the points. If -dist is used, -num will be ignored", default=0)
+# function to create and write the points to files
+def points_creator(data_list):
+    print(f"Grid File Creator: Writing on output file/s")
 
-(options, args) = parser.parse_args()
+    for (chromos, A, B, num_intervals) in data_list:
+        fileout_name = "points." + chromos + "." + ".".join(map(str, [A, B, num_intervals])) + ".out"
+        
+        # Generate the points betweem A and B
+        num_intervals    = num_intervals + 1 
+        step_size        = (B - A) / num_intervals
 
-chr=options.chr    #chromosome
-numOfPoints=int(options.num)
-rangeEnd=int(options.end)
-rangeStart=int(options.start)
-userdist=int(options.dist)
+        with open(fileout_name,"w") as file1:
+            file1.write(f"chr{chromos}\t{A}\n")
+            for i in range(1, num_intervals):
+                point = int(A + i * step_size)
+                file1.write(f"chr{chromos}\t{point}\n")
+            file1.write(f"chr{chromos}\t{B}\n")
 
-diff=rangeEnd-rangeStart+2
-if(diff<=0):
-    print "Error! Please check your input"
-if(userdist == 0 and numOfPoints==0):
-    print "Error! Please check your input"
-else:
-    if(userdist != 0):
-        step=userdist
-        numOfPoints=(diff/step) +1
-    else:
-        step = (diff / (numOfPoints - 1));  upoloipo=diff%(numOfPoints - 1)
-        if (upoloipo > 5):
-            step = step + 1
-        if (step < 1):
-            step = 1
-    x=rangeStart
-    positions=[]
+def print_error():
+    print(f"Something went wrong")
+    return 0
 
-    test=range(rangeStart, rangeEnd+1,step)
-    for item in test:
-        if (positions.count(item) == 0 and len(positions)<(numOfPoints-1)):
-            positions.append(item)
+def main():
+    print(f"Grid File Creator")
+    data_list = []
+    print(f"Grid File Creator: Reading input file")
+    # check the input file if the data is valid
+    with open(INFILE,'r') as file0:
+        for line in file0:
+            tokens = line.split('\n')[0].split(' ')
+            if len(tokens) != 4:
+                return print_error()
+            if False in list(filter(lambda x:x.isdigit(),tokens)):
+                return print_error()
+            chromosome  = tokens[0]
+            rangeStart  = int(tokens[1])
+            rangeEnd    = int(tokens[2])
+            numOfPoints = int(tokens[3])
 
-    if(positions.count(rangeEnd)==0):
-        positions.append(rangeEnd)
+            # check input validity
+            if rangeEnd < rangeStart or numOfPoints == 0 or not chromosome.isdigit():
+                return print_error()
 
-    # sort positions
-    positions.sort()
+            data_list.append((chromosome, rangeStart, rangeEnd, numOfPoints))
+    
+    points_creator(data_list)
+    print(f"Grid File Creator: Creating points completed")
+    return 1
 
-    #time to export our data
-    outputfile="points."+chr+"."+str(rangeStart)+"."+str(rangeEnd)+".out"
-    outFile = open(outputfile, "w")
-    # print positions
-    for item in positions:
-        outFile.write("chr" + chr+"\t" + str(item) + "\n")
-    outFile.close()
-
-    #print statistics for debugging
-    print "[Creating points completed]"
-    print "Area: "+str(rangeStart)+" - "+str(rangeEnd)
-    print "Distance:" + str(step)
-    print "[DEBUGGING MSG] "+str(len(positions))+"/"+str(numOfPoints)+" points created and exported to " +outputfile
+if __name__ == "__main__":
+        main()
